b69c4c356fe369b6e89154fa900220abfa6889f8
Mming-Lab <166465033+Mming-Lab@users.noreply.github.com>
2024-05-26 02:13:30 +0900

    更新 main.blocks, main.ts, custom/agent_custom.ts, custom/turnDirectionImg.jres

--- custom/agent_custom.ts
+++ custom/agent_custom.ts
@@ -20,6 +20,15 @@ declare const enum SixDirectionImg {
     Down = SixDirection.Down
 }
 
+declare const enum TurnDirectionImg {
+    //% block="10:左(ひだり)"
+    //% jres alias = ""
+    Left = TurnDirection.Left,
+    //% block="20:右(みぎ)"
+    //% jres alias = ""
+    Right = TurnDirection.Right
+}
+
 namespace agent {
 
     //% blockGap=8
@@ -32,27 +41,31 @@ namespace agent {
     }
 
     //% weight=500
-    //% block="01:`SixDirectionImg.Forward`前(まえ)に 1 ブロック移動(いどう)"
+    //% block="01:`SixDirectionImg.Forward`に 1 ブロック移動(いどう)"
     export function moveFrontOne(): void {
         agent.move(FORWARD, 1);
     }
     //% weight=490
-    //% block="02:`TurnDirectionImg.LeftTurn`向(む)きを左(ひだり)に変(か)える"
+    //% block="02:向(む)きを`TurnDirectionImg.Left`に変(か)える"
     export function turn_Left(): void {
         agent.turn(LEFT_TURN);
     }
     //% weight=480
-    //% block="03:`TurnDirectionImg.RightTurn`向(む)きを右(みぎ)に変(か)える"
+    //% block="03:向(む)きを`TurnDirectionImg.Right`に変(か)える"
     export function turn_Right(): void {
         agent.turn(RIGHT_TURN)
     }
     //% weight=470
-    //% block="04:エージェントを%direction|に%blocks|ブロック移動(いどう)させる"
+    //% block="04:%direction|に%blocks|ブロック移動(いどう)"
     //% blocks.defl=1
     export function moveImg(direction: SixDirectionImg, blocks:number): void {
         agent.move(direction, blocks);
     }
-
+    //% weight=450
+    //% block="05:向(む)きを%direction|に変(か)える"
+    export function turnImg(direction: TurnDirectionImg): void {
+        agent.turn(direction);
+    }
 
 
 }
