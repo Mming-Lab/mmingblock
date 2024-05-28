3e9de66c41f7ec1fa1c2bacbba2145257871f843
Mming-Lab <166465033+Mming-Lab@users.noreply.github.com>
2024-05-28 18:06:04 +0900

    更新 pxt.json, main.blocks, main.ts, custom/agent_custom.ts

--- custom/agent_custom.ts
+++ custom/agent_custom.ts
@@ -1,52 +1,52 @@
 
 declare const enum SixDirectionImg {
-    //% block="10:前(まえ)"
+    //% block="まえ"
     //% jres alias = ""
     Forward = SixDirection.Forward,
-    //% block="20:後(うし)ろ"
+    //% block="うしろ"
     //% jres alias = ""
     Back = SixDirection.Back,
-    //% block="30:左(ひだり)"
+    //% block="ひだり"
     //% jres alias = ""
     Left = SixDirection.Left,
-    //% block="40:右(みぎ)"
+    //% block="みぎ"
     //% jres alias = ""
     Right = SixDirection.Right,
-    //% block="50:上(うえ)"
+    //% block="うえ"
     //% jres alias = ""
     Up = SixDirection.Up,
-    //% block="60:下(した)"
+    //% block="した"
     //% jres alias = ""
     Down = SixDirection.Down
 }
 
 declare const enum TurnDirectionImg {
-    //% block="10:左(ひだり)"
+    //% block="ひだり"
     //% jres alias = ""
     Left = TurnDirection.Left,
-    //% block="20:右(みぎ)"
+    //% block="みぎ"
     //% jres alias = ""
     Right = TurnDirection.Right
 }
 
 namespace agent {
     //% weight=500
-    //% block="01:前(まえ)`SixDirectionImg.Forward`に 1 ブロック移動(いどう)"
+    //% block="`SixDirectionImg.Forward`に いっぽ すすむ"
     export function moveFrontOne(): void {
         agent.move(FORWARD, 1);
     }
     //% weight=490
-    //% block="02:向(む)きを左(ひだり)`TurnDirectionImg.Left`に変(か)える"
+    //% block="`TurnDirectionImg.Left`を むく"
     export function turn_Left(): void {
         agent.turn(LEFT_TURN);
     }
     //% weight=480
-    //% block="03:向(む)きを右(みぎ)`TurnDirectionImg.Right`に変(か)える"
+    //% block="`TurnDirectionImg.Right`を むく"
     export function turn_Right(): void {
         agent.turn(RIGHT_TURN)
     }
     //% weight=470
-    //% block="04:%direction|に%blocks|ブロック移動(いどう)"
+    //% block="%direction|に%blocks|っぽ すすむ"
     //% direction.fieldEditor="gridpicker"
     //% direction.fieldOptions.width=160
     //% blocks.defl=1   
@@ -54,7 +54,7 @@ namespace agent {
         agent.move(direction, blocks);
     }
     //% weight=450
-    //% block="05:向(む)きを%direction|に変(か)える"
+    //% block="%direction|を むく"
     //% direction.fieldEditor="gridpicker"
     //% direction.fieldOptions.width=80
     export function turnImg(direction: TurnDirectionImg): void {
--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ loops.forever(function () {
     agent.moveFrontOne()
     agent.turn_Left()
     agent.turn_Right()
-    agent.moveImg(SixDirectionImg.Forward, 1)
     agent.turnImg(TurnDirectionImg.Left)
+    agent.moveImg(SixDirectionImg.Forward, 1)
 })
