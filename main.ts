2da6fb308b6809773e6504369bff0ca58cc842af
Mming-Lab <166465033+Mming-Lab@users.noreply.github.com>
2024-05-29 10:48:47 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-loops.forever(function () {
-    agent.moveFrontOne()
-    agent.moveOneImg(SixDirectionImg.Forward)
-    agent.moveImg(SixDirectionImg.Forward, 1)
-    agent.turn_Left()
-    agent.turn_Right()
-    agent.turnImg(TurnDirectionImg.Left)
-})
+
